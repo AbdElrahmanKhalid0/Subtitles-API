e2b8f34f05d052771ef227dc95222c81f8a61026
Abdelrahman Khalid <abdelrahman.elzohry2014@gmail.com>
2020-07-03 20:22:26 +0200

    added the ability to get the subtitles from a title page

--- api.py
+++ api.py
@@ -57,7 +57,7 @@ def get_matches(title: str, search_type: str = "all", backup_search_type: str = 
         
         titles_subtitles_anchors = h2_search_type.find_next("ul").find_all("a")  
             
-    titles_subtitles_texts = remove_dublicates([anchor.text for anchor in titles_subtitles_anchors])
+    titles_subtitles_texts = remove_dublicates([anchor.text.strip() for anchor in titles_subtitles_anchors])
     titles_subtitles_links = [URL_SITE + anchor.get("href") for anchor in titles_subtitles_anchors]
     
     return dict(zip(titles_subtitles_texts, titles_subtitles_links))
@@ -87,6 +87,7 @@ def get_exact_match(title: str, year: str) -> str:
 
     return match, url
 
+
 def search_by_title(title: str) -> list:
     """gets the url to the title subtitle page, and returns None in case error happened"""
     
@@ -105,6 +106,61 @@ def search_by_title(title: str) -> list:
     # find
 
 
+class Subtitle:
+    def __init__(self, htmlNode):
+        self.setAttrs(htmlNode)
+
+    def setAttrs(self, htmlNode):
+        self.link = URL_SITE + htmlNode.find("td", class_="a1").find("a")["href"]
+        self.language = htmlNode.select("span.l.r")[0].text.strip()
+        self.title = htmlNode.find_all("span")[1].text.strip()
+        self.kind = self.get_subtitle_kind(self.title)
+
+    def get_subtitle_kind(self, sub_title):
+        """gets the kind of the subtitle (bluray, hdrip, web-dl, trailer, ...)
+        and returns None in case of unknown type"""
+        
+        # the following RegEx are for finding the blueray or the blue-ray
+        if "trailer" in sub_title.lower():
+            return "trailer"
+        elif re.compile("blu?[\s\S]ray").match(sub_title, re.IGNORECASE):
+            return "blueray"
+        elif re.compile("hd?[\s\S]rip").match(sub_title, re.IGNORECASE):
+            return "hdrip"
+        elif re.compile("web?[\s\S]dl").match(sub_title, re.IGNORECASE):
+            return "web-dl"
+        elif re.compile("hd?[\s\S]ts").match(sub_title, re.IGNORECASE):
+            return "hd-ts"
+        elif re.compile("hd?[\s\S]tc").match(sub_title, re.IGNORECASE):
+            return "hd-tc"
+        elif re.compile("web?[\s\S]rip").match(sub_title, re.IGNORECASE):
+            return "webrip"
+        elif re.compile("br?[\s\S]rip").match(sub_title, re.IGNORECASE):
+            return "brrip"
+        elif re.compile("bd?[\s\S]rip").match(sub_title, re.IGNORECASE):
+            return "bdrip"
+        else:
+            return None
+        
+
+def get_subtitles(title_page_url: str, languages: list = ['en']) -> list:
+    """gets the subtitles from a specific title page"""
+
+    page = requests.get(title_page_url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    check_soup(soup)
+
+    # getting the subtitles nodes and then removing the empty ones from it
+    subtitles_nodes = soup.find("div", class_="content clearfix")\
+        .find("table").find("tbody").find_all("tr")
+    subtitles_nodes = [subtitle_node for subtitle_node in subtitles_nodes if subtitle_node.find("a")]
+    
+    subtitles = [Subtitle(subtitle_node) for subtitle_node in subtitles_nodes]
+
+    return subtitles
+    
+
+
 def main():
     title = get_args(argv)
 
@@ -113,5 +169,6 @@ if __name__ == "__main__":
     # main()
     # search_by_title("avengers")
     # print(get_matches("avengers", search_type="popular"))
-    print(get_exact_match("avengers endgame", "2019"))
+    # print(get_exact_match("avengers endgame", "2019"))
+    get_subtitles(get_exact_match("avengers endgame", "2019")[1])
     # print(get_year("Avengers: Endgame (2019)"))
